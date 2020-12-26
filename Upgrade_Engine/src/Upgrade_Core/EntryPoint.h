316e79f66ebc3b51139535251a0cb0b54e246085
Short1398 <short1398@gmail.com>
2020-12-26 00:16:49 -0500

    Implemented a basic logging library as a wrapper for an exisiting logging sub-module from gabime/spdlog. Created a core and client logger, string pattern for printing, and pre-defined macros readability, use, and optimization

--- Upgrade_Engine/src/Upgrade_Core/EntryPoint.h
+++ Upgrade_Engine/src/Upgrade_Core/EntryPoint.h
@@ -5,7 +5,14 @@
 	extern UpgradeE::Application* UpgradeE::CreateApplication();
 	int main(int argc, char** argv)
 	{
-		printf("Welcome to Upgrade Engine");
+		printf("Welcome to Upgrade Engine\n:");
+
+		UpgradeE::Log::Init();
+		HZ_CORE_ERROR("Working log from core");
+		int a = 5;
+		HZ_INFO("Working log from core Var={0}", a);
+
+
 		auto app = UpgradeE::CreateApplication();
 		app->Run();
 		delete app;
